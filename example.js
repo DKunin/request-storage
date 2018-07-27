a06b4542081eab521b9637133a23d645608a6e54
Dmitry Kunin <dkunin@avito.ru>
2018-07-27 13:11:36 +0300

    Added logging, debouncing if cache is not synced

--- example.js
+++ example.js
@@ -7,7 +7,25 @@ const resultNode = document.querySelector('.result');
 
 requestStorage
     .getFeature('http://www.mocky.io/v2/5b59e7232f0000aa2a5f963c')
-    .then(console.log);
+    .then(result => {
+        console.log('result 1', result);
+    });
+
+requestStorage
+    .getFeature('http://www.mocky.io/v2/5b5ae6e73100005c009a8930')
+    .then(result => {
+        console.log('result 2', result);
+    });
+
+const buttonNode = document.querySelector('button');
+
+buttonNode.addEventListener('click', function() {
+    requestStorage
+        .getFeature('http://www.mocky.io/v2/5b5ae5e13100002d119a892b')
+        .then(result => {
+            console.log('result 3', result);
+        });
+});
 
 setInterval(function() {
     resultNode.innerText = JSON.stringify(requestStorage.getFeatures());
--- index.js
+++ index.js
@@ -1,12 +1,27 @@
-function logging(...rest) {
-    console.log(rest);
+'use strict';
+
+const SESSION_STORAGE_KEY = 'sessionStorageItem';
+const GET_SESSION_STORAGE_KEY = 'getSessionStorage';
+let env;
+try {
+    env = process.env;
+} catch (err) {
+    env = {};
+}
+const PRODUCTION = !env ? false : env.NODE_ENV === 'production';
+
+function logging() {
+    if (PRODUCTION) {
+        return null;
+    }
+    console.log([].slice.call(arguments));
 }
 
 let requestStorage = {
     addRequest(key, value) {
         this.urls[key] = value;
         sessionStorage.setItem(
-            'sessionStorageItem',
+            SESSION_STORAGE_KEY,
             JSON.stringify(requestStorage.urls)
         );
     },
@@ -14,33 +29,44 @@ let requestStorage = {
         logging('sync', savedState);
         this.urls = savedState || {};
     },
+    fetchFeature(url, resolve, reject) {
+        logging('fetching request');
+        fetch(url, {
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json'
+            },
+            credentials: 'same-origin'
+        })
+            .then(response => response.json())
+            .then(result => {
+                this.addRequest(url, result);
+                resolve(result);
+            })
+            .catch(error => {
+                reject(error);
+                throw new Error(error);
+            });
+    },
     getFeature(url) {
         return new Promise((resolve, reject) => {
             if (!url) {
                 reject({ error: 'Please provide url' });
             }
             logging(this.urls);
+
             if (this.urls && this.urls[url]) {
                 logging('fetching request from cache');
                 return resolve(this.urls[url]);
             }
-            logging('fetching request');
-            fetch(url, {
-                headers: {
-                    Accept: 'application/json',
-                    'Content-Type': 'application/json'
-                },
-                credentials: 'same-origin'
-            })
-                .then(response => response.json())
-                .then(result => {
-                    this.addRequest(url, result);
-                    resolve(result);
-                })
-                .catch(error => {
-                    reject(error);
-                    throw new Error(error);
-                });
+
+            if (!this.synced) {
+                setTimeout(() => {
+                    this.fetchFeature(url, resolve, reject);
+                }, 0);
+            } else {
+                this.fetchFeature(url, resolve, reject);
+            }
         });
     },
     getFeatures() {
@@ -51,36 +77,37 @@ let requestStorage = {
     },
     purge() {
         this.urls = {};
-        localStorage.removeItem('sessionStorageItem');
-        sessionStorage.removeItem('sessionStorageItem');
+        localStorage.removeItem(SESSION_STORAGE_KEY);
+        sessionStorage.removeItem(SESSION_STORAGE_KEY);
     },
     init() {
         requestStorage.sync(
-            JSON.parse(sessionStorage.getItem('sessionStorageItem')) || {}
+            JSON.parse(sessionStorage.getItem(SESSION_STORAGE_KEY)) || {}
         );
 
         if (requestStorage.isEmpty()) {
-            localStorage.setItem('getSessionStorage', Date.now());
+            localStorage.setItem(GET_SESSION_STORAGE_KEY, Date.now());
         }
 
         window.addEventListener('storage', function(event) {
             logging('event', event, requestStorage.isEmpty());
-            if (event.key == 'getSessionStorage') {
+            if (event.key == GET_SESSION_STORAGE_KEY) {
                 localStorage.setItem(
-                    'sessionStorageItem',
+                    SESSION_STORAGE_KEY,
                     JSON.stringify(requestStorage.urls)
                 );
-                localStorage.removeItem('sessionStorageItem');
+                localStorage.removeItem(SESSION_STORAGE_KEY);
             } else if (
-                event.key == 'sessionStorageItem' &&
+                event.key == SESSION_STORAGE_KEY &&
                 requestStorage.isEmpty()
             ) {
-                var data = JSON.parse(event.newValue);
-                requestStorage.sync(data);
+                requestStorage.sync(JSON.parse(event.newValue));
+                this.synced = true;
             }
         });
     },
-    urls: {}
+    urls: {},
+    synced: false
 };
 
 export default requestStorage;
